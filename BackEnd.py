64dc75838a09ff7b5e75f94e4fc0ed8c4b252645
Gabriel henrique gemelli <126275774+Gabriel-Henrique-Gemelli@users.noreply.github.com>
2024-11-19 14:13:09 -0300

    criaçao do back end

--- BackEnd.py
+++ BackEnd.py
@@ -6,6 +6,9 @@ baud_rate = 9600
 
 ser = None
 
+# Variáveis para armazenamento dos dados de pressão
+pressao = None
+
 try:
     ser = serial.Serial(serial_port, baud_rate, timeout=1)
     print("Conexão estabelecida com o Arduino!")
@@ -14,6 +17,13 @@ try:
         if ser.in_waiting > 0:
             time.sleep(5)
             data = ser.readline().decode('utf-8').strip()
+
+            if "Pressao:" in data:
+                pressao = float(data.split(":")[1].strip())
+            
+            if pressao is not None:
+                print(f"Pressão: {pressao} hPa")
+            
 
 except serial.SerialException:
     print("Não foi possível conectar ao Arduino.")
